4a8e487bfd2bb6e7e4f04bff8f45617c63a36485
priyanshujhagithub <129042608+priyanshujhagithub@users.noreply.github.com.>
2025-05-09 23:05:08 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import { ParallaxProvider } from "react-scroll-parallax";
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import About from './components/About';
+import StorySection from './components/StorySection';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ParallaxProvider>
+      <Navbar />
+      <Hero />
+      <About />
+      <StorySection />
+      <Projects />
+      <Contact />
+      <Footer />
+    </ParallaxProvider>
   );
 }
 
---
+++ src/components/Hero.jsx
@@ -0,0 +1,10 @@
+import {Parallax} from 'react-scroll-parallax';
+
+const Hero = ()=>(
+    <Parallax speed={-20}>
+        <div className="hero-section">
+            <h1>Hello, I'm Priyanshu Jha</h1>
+            <p>Software Developer</p>
+        </div>
+    </Parallax>
+);
